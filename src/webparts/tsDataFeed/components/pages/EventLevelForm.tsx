10695dea939af189e2454370fdd0c72cf1c82d98
Bharat Bhatia <bharat.bhatia_c@leadschool.in>
2022-12-29 02:14:49 +0530

    axios integration

--- src/webparts/tsDataFeed/components/pages/EventLevelForm.tsx
+++ src/webparts/tsDataFeed/components/pages/EventLevelForm.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
+import {useEffect} from "react";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import InputGroup from "react-bootstrap/InputGroup";
 import Card from "react-bootstrap/Card";
+import AxiosInstance from "../../services/AxiosInstance";
 
 const EventLevelForm = () => {
+
+  useEffect(()=>{
+    AxiosInstance.get('/posts').then((res)=>{
+      console.log(res)
+    })
+  },[])
 
   const submitHandler = (e: any) => {
     e.preventDefault();
---
+++ src/webparts/tsDataFeed/services/AxiosInstance.tsx
@@ -0,0 +1,10 @@
+
+import axios from 'axios';
+
+const AxiosInstance = axios.create({
+    baseURL: 'https://my-json-server.typicode.com/typicode/demo'
+});
+
+
+
+export default AxiosInstance
